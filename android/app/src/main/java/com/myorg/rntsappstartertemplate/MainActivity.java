d6f84e2087bc73e3343a3663fac58dba8c225493
Kaybarax <barasakevin1@gmail.com>
2020-10-24 01:50:35 +0300

    Complete app rename

--- android/app/src/main/java/com/myorg/rntsappstartertemplate/MainActivity.java
+++ android/app/src/main/java/com/myorg/rntsappstartertemplate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativetsappstartertemplate;
+package com.myorg.rntsappstartertemplate;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeTsAppStarterTemplate";
+    return "RNTSAppStarterTemplate";
   }
 }
--- ios/RNTSAppStarterTemplateTests/RNTSAppStarterTemplateTests.m
+++ ios/RNTSAppStarterTemplateTests/RNTSAppStarterTemplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeTsAppStarterTemplateTests : XCTestCase
+@interface RNTSAppStarterTemplateTests : XCTestCase
 
 @end
 
-@implementation ReactNativeTsAppStarterTemplateTests
+@implementation RNTSAppStarterTemplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
